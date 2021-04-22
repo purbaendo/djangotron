4d36866da900ad5a16a4f80f63b60dd2d795e350
Endo <endogeo06@gmail.com>
2021-04-22 14:34:26 +0700

    Commit

--- tutorial/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<pagename>/', views.topic, name='topic'),
-    path('<pagename>/<title_post>', views.post_detail, name='post_detail'),
-]
-
