6155db15ecd1d52a37045237e3bb6cfe366bc042
Endo <endogeo06@gmail.com>
2021-04-22 16:06:08 +0700

    commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Dokumentaris.wsgi --log-file -
--- static/js/editor.js
+++
@@ -1,5 +0,0 @@
-function updateIframe(){
-  var myFrame = $("#myframe").contents().find('body');
-  var textareaValue = $("textarea").val();
-  myFrame.html(textareaValue);
-}
